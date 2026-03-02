82ee73e58bdac52cd161d24aae10789c35bcc34c
Raizon Asis <raizon@Raizons-MacBook-Pro.local>
2026-03-02 19:24:53 +0800

    Lab3

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import recipe_list, recipe_detail
+from . import views
 
 urlpatterns = [
-    # Path for the list of recipes
+    path('', views.recipe_list, name='home'),
     path('recipes/list/', recipe_list, name='recipe_list'),
-    # Path for a specific recipe detail
     path('recipe/<int:pk>/', recipe_detail, name='recipe_detail'),
 ]
