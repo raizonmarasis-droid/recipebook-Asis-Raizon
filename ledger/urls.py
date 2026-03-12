ccf63e2b23f44276b7929f7fb2d32452e124a663
Raizon Asis <raizon@Raizons-MacBook-Pro.local>
2026-03-12 17:35:41 +0800

    Implement RecipeCreateView and RecipeImageCreateView with redirects

--- ledger/urls.py
+++ ledger/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.recipe_list, name='home'),
     path('recipes/list/', recipe_list, name='recipe_list'),
     path('recipe/<int:pk>/', recipe_detail, name='recipe_detail'),
+    path('recipe/<int:pk>/add_image/', views.RecipeImageCreateView.as_view(), name='recipe_add_image'),
 ]
--- ledger/views.py
+++ ledger/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Recipe
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Recipe, RecipeImage, Profile
+
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    return render(request, 'recipe_list.html', {'recipes': recipes})
+    return render(request, 'recipe_list.html', {'recipe_list': recipes})
 
-@login_required
+
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipe_detail.html', {'recipe': recipe})
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['name'] # Form only shows name field 
+    template_name = 'recipe_form.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('recipe_list')
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    fields = ['image', 'description'] 
+    template_name = 'recipe_image_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.kwargs['pk']}) [cite: 31]
+
+    def form_valid(self, form):
+        form.instance.recipe = get_object_or_404(Recipe, pk=self.kwargs['pk'])
+        return super().form_valid(form)
