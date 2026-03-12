41e809ffc746f7302f0d19f6513c0b52ed2c9db8
Raizon Asis <raizon@Raizons-MacBook-Pro.local>
2026-03-12 23:18:31 +0800

    Changed the message when adding an image

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.recipe_list, name='home'),
     path('recipes/list/', recipe_list, name='recipe_list'),
+
+    path('recipe/add/', views.RecipeCreateView.as_view(), name='recipe_add'),
     path('recipe/<int:pk>/', recipe_detail, name='recipe_detail'),
     path('recipe/<int:pk>/add_image/', views.RecipeImageCreateView.as_view(), name='recipe_add_image'),
 ]
